61d08ee3cb3b18edd21047d5a1c187a8dbd35f4e
Andy Wang <66895883+andy1197@users.noreply.github.com>
2022-02-16 22:59:06 -0800

    login template

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h3> Register </h3>
+        <input placeholder="Email" />
+        <input placeholder="Password" />
+        <button> Create User </button>
+      </div>
+
+      <div>
+        <h3> Login </h3>
+        <input placeholder="Email" />
+        <input placeholder="Password" />
+        <button> Login </button>
+      </div>
+
+      <div>
+        <h3> User Logged In: </h3>
+        <button>Sign Out</button>
+      </div>
+
     </div>
   );
 }
