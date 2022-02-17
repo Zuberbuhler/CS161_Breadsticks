3b9091beb499f652160673508df7a8ce938c3abc
Andy Wang <66895883+andy1197@users.noreply.github.com>
2022-02-16 23:45:52 -0800

    barebones login functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,97 @@
-import './App.css';
+import { useState } from "react";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  onAuthStateChanged,
+  signOut,
+} from "firebase/auth";
+import "./App.css";
+import { auth } from "./firebase";
 
 function App() {
+
+  const [registerEmail, setRegisterEmail] = useState("");
+  const [registerPassword, setRegisterPassword] = useState("");
+  const [loginEmail, setLoginEmail] = useState("");
+  const [loginPassword, setLoginPassword] = useState("");
+
+  const [user, setUser] = useState({})
+
+  onAuthStateChanged(auth, (currentUser) => {
+    setUser(currentUser);
+  })
+
+  const register = async () => {
+    try {
+      // creates a user and logs in automatically
+      console.log("hello");
+      const user = await createUserWithEmailAndPassword(
+        auth,
+        registerEmail,
+        registerPassword
+      );
+      console.log(user);
+    } 
+    catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  const login = async () => {
+    try {
+      // creates a user and logs in automatically
+      console.log("hello");
+      const user = await signInWithEmailAndPassword(
+        auth,
+        loginEmail,
+        loginPassword
+      );
+      console.log(user);
+    } 
+    catch (error) {
+      console.log(error.message);
+    }
+  }
+
+  const logout = async () => {
+    await signOut(auth);
+  }
+
   return (
     <div className="App">
       <div>
-        <h3> Register </h3>
-        <input placeholder="Email" />
-        <input placeholder="Password" />
-        <button> Create User </button>
+        <h3>Register</h3>
+        <input placeholder="Email" 
+        onChange={(event) => {
+          setRegisterEmail(event.target.value);
+        }}
+        />
+        <input placeholder="Password" 
+        onChange={(event) => {
+          setRegisterPassword(event.target.value);
+        }}
+        />
+        <button onClick={register}>Create User</button>
       </div>
 
       <div>
-        <h3> Login </h3>
-        <input placeholder="Email" />
-        <input placeholder="Password" />
-        <button> Login </button>
+        <h3>Login</h3>
+        <input placeholder="Email" 
+        onChange={(event) => {
+          setLoginEmail(event.target.value);
+        }}
+        />
+        <input placeholder="Password" 
+        onChange={(event) => {
+          setLoginPassword(event.target.value);
+        }}
+        />
+        <button onClick={login}>Login</button>
       </div>
 
       <div>
-        <h3> User Logged In: </h3>
-        <button>Sign Out</button>
+        <h3>User Logged In:{user?.email}</h3>
+        <button onClick={logout}>Sign Out</button>
       </div>
 
     </div>
--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,15 @@
-import firebase from 'firebase/compat/app'
-import 'firebase/compat/auth'
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 
-const app = firebase.initializeApp({
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_APP_ID
-})
+const firebaseConfig = {
+    apiKey: "AIzaSyCmC8jAo0ZctD8Z5iLbCtZ7_pGwGIpEbQ4",
+    authDomain: "breadsticks-de840.firebaseapp.com",
+    projectId: "breadsticks-de840",
+    storageBucket: "breadsticks-de840.appspot.com",
+    messagingSenderId: "1007944052536",
+    appId: "1:1007944052536:web:7c429f423b63fc6e0a5bcd"
+};
 
-export const auth = app.auth
-export default app
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
